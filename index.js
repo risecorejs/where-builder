00ebcd395cc9ab6aa10071957ee2ca3557041912
amenov <amenov.abdulsalam@gmail.com>
2022-03-27 17:33:40 +0600

    update

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const _ = require('lodash')
+
+/**
+ * WHERE-BUILDER
+ * @param query {Object}
+ * @param abstractions {any[]}
+ * @returns {Object}
+ */
 module.exports = (query, abstractions) => {
   const where = {}
 
@@ -5,62 +13,66 @@ module.exports = (query, abstractions) => {
     if (typeof abstraction === 'string') {
       const whereKey_queryKey = abstraction
 
-      if (query[whereKey_queryKey]?.length) {
-        where[whereKey_queryKey] = query[whereKey_queryKey]
+      const { has, value } = getValueByQueryKey(query, whereKey_queryKey)
+
+      if (has) {
+        where[whereKey_queryKey] = value
       }
     } else if (Array.isArray(abstraction)) {
-      switch (abstraction.length) {
-        case 2:
-          {
-            const secondElement = abstraction[1]
+      if (abstraction.length === 2) {
+        const secondElement = abstraction[1]
 
-            if (typeof secondElement === 'string') {
-              const [whereKey, queryKey] = abstraction
+        if (typeof secondElement === 'string') {
+          const [whereKey, queryKey] = abstraction
 
-              if (query[queryKey]?.length) {
-                where[whereKey] = query[queryKey]
-              }
-            } else if (secondElement.constructor === Object) {
-              const [whereKey_queryKey, object] = abstraction
+          const { has, value } = getValueByQueryKey(query, queryKey)
 
-              if (query[whereKey_queryKey]?.length) {
-                where[whereKey_queryKey] = object
-              }
-            } else if (typeof secondElement === 'function') {
-              const [whereKey, handler] = abstraction
+          if (has) {
+            where[whereKey] = value
+          }
+        } else if (secondElement.constructor === Object) {
+          const [whereKey_queryKey, object] = abstraction
 
-              const result = handler(query[whereKey])
+          const { has } = getValueByQueryKey(query, whereKey_queryKey)
 
-              if (result !== void 0) {
+          if (has) {
+            where[whereKey_queryKey] = object
+          }
+        } else if (typeof secondElement === 'function') {
+          const [whereKey, handler] = abstraction
+
+          const { value } = getValueByQueryKey(query, whereKey)
+
+          const result = handler(value)
+
+          if (result !== void 0) {
+            where[whereKey] = result
+          }
+        }
+      } else if (abstraction.length === 3) {
+        const [whereKey, queryKey, objectOrHandler] = abstraction
+
+        const { has, value } = getValueByQueryKey(query, queryKey)
+
+        if (has) {
+          if (objectOrHandler.constructor === Object) {
+            if (whereKey === null) {
+              Object.assign(where, objectOrHandler)
+            } else {
+              where[whereKey] = objectOrHandler
+            }
+          } else if (typeof objectOrHandler === 'function') {
+            const result = objectOrHandler(value)
+
+            if (result !== void 0) {
+              if (whereKey === null) {
+                Object.assign(where, result)
+              } else {
                 where[whereKey] = result
               }
             }
           }
-          break
-
-        case 3:
-          const [whereKey, queryKey, objectOrHandler] = abstraction
-
-          if (query[queryKey]?.length) {
-            if (objectOrHandler.constructor === Object) {
-              if (whereKey === null) {
-                Object.assign(where, objectOrHandler)
-              } else {
-                where[whereKey] = objectOrHandler
-              }
-            } else if (typeof objectOrHandler === 'function') {
-              const result = objectOrHandler(query[queryKey])
-
-              if (result !== void 0) {
-                if (whereKey === null) {
-                  Object.assign(where, result)
-                } else {
-                  where[whereKey] = result
-                }
-              }
-            }
-          }
-          break
+        }
       }
     } else if (abstraction.constructor === Object) {
       Object.assign(where, abstraction)
@@ -68,4 +80,31 @@ module.exports = (query, abstractions) => {
   }
 
   return where
+}
+
+/**
+ * GET-VALUE-BY-QUERY-KEY
+ * @param query {Object}
+ * @param key {string}
+ * @returns {{
+ *   has: boolean,
+ *   value: void|any
+ * }}
+ */
+function getValueByQueryKey(query, key) {
+  const result = {
+    has: false,
+    value: query[key]
+  }
+
+  if (
+    result.value?.length ||
+    ['number', 'boolean'].includes(typeof result.value) ||
+    result.value === null ||
+    (result.value?.constructor === Object && !_.isEmpty(result.value))
+  ) {
+    result.has = true
+  }
+
+  return result
 }
